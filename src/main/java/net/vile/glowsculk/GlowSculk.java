99046f0b3cf61438cac6787cfd375ce0d2d26ee3
antro <antrodriguezer@gmail.com>
2022-06-18 12:26:55 -0500

    Initial Commit

---
+++ src/main/java/net/vile/glowsculk/GlowSculk.java
@@ -0,0 +1,15 @@
+package net.vile.glowsculk;
+
+import net.fabricmc.api.ModInitializer;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class GlowSculk implements ModInitializer {
+	public static final String MOD_ID = "glowsculk";
+	public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
+
+	@Override
+	public void onInitialize() {
+
+	}
+}
