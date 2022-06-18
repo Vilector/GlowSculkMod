f9a9f2ec3218816665603563621f169ca018bdfa
antro <antrodriguezer@gmail.com>
2022-06-18 12:28:13 -0500

    Initial Commit

---
+++ src/main/java/net/vile/glowsculk/GlowSculkClient.java
@@ -0,0 +1,10 @@
+package net.vile.glowsculk;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class GlowSculkClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
